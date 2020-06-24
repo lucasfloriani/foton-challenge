c1c8d83f7f1b2619e5d4dc15622fd20f509b41fe
Lucas Alexander Floriani <lucas.floriane@gmail.com>
2020-06-24 16:23:20 -0300

    feat(server): add jwt env vars to .env files

--- packages/server/src/common/config.ts
+++ packages/server/src/common/config.ts
@@ -18,7 +18,6 @@ if (!process.env.NOW_REGION) {
 // Server
 export const GRAPHQL_PORT = envVar.get('GRAPHQL_PORT', '5001').asPortNumber();
 
-// TODO: Add info about those env vars
 export const REFRESH_TOKEN_SECRET = envVar.get('REFRESH_TOKEN_SECRET').asString();
 export const ACCESS_TOKEN_SECRET = envVar.get('ACCESS_TOKEN_SECRET').asString();
 
